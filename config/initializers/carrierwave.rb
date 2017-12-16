b20b54650badacbe023548ce3e1bb7cf5b56edca
dzhiqin <dzhiqin@126.com>
2017-12-16 17:29:34 +0800

    修改七牛bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["iDMZ1ndGSH9_DFemEjmGLmo-jKSJzXqeKQOEkjSv"]
-  config.qiniu_secret_key    = ENV["jTypK5yuc3P2sL-g-02BoWEzQscBsR_8LqlPgkrz"]
-  config.qiniu_bucket        = ENV["goodjob"]
-  config.qiniu_bucket_domain = ENV["p11nkypcn.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
